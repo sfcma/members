f7897d1386c26ba903e630bca4f00cac43dc5af5
Dan Brewster <dan@dbrews.com>
2016-09-09 20:05:32 -0700

    Hook up start of authentication, start homepage

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :member_instruments
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909234900) do
+ActiveRecord::Schema.define(version: 20160909234910) do
 
   create_table "absences", force: :cascade do |t|
     t.integer  "member_id"
@@ -113,6 +113,13 @@ ActiveRecord::Schema.define(version: 20160909234900) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
