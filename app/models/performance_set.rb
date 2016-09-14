d92311d0c180103fa53a1ed4a0e0ec66c33d110f
Dan Brewster <dan@dbrews.com>
2016-09-13 23:56:44 -0700

    Just kidding, we want to allow addition of historical sets

--- app/models/performance_set.rb
+++ app/models/performance_set.rb
@@ -4,8 +4,8 @@ class PerformanceSet < ApplicationRecord
   validates :ensemble_id, presence: true
   validates :name, presence: true, format: { with: /.*[0-9]{4}.*/,
                                              message: "invalid -- must include a year"}
-  validates_date :start_date, on_or_after: 1.year.ago, on_or_before: 5.years.from_now
-  validates_date :end_date, on_or_after: 1.year.ago, on_or_before: 5.years.from_now
+  validates_date :start_date, on_or_before: 5.years.from_now
+  validates_date :end_date, on_or_before: 5.years.from_now
 
   def old_name
     return "#{Ensemble.find(ensemble_id).name} from #{start_date} to #{end_date}"
