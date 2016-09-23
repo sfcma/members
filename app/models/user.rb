a6a26cbc01300e19eca621ba27ba399b8732c16a
Dan Brewster <dan@dbrews.com>
2016-09-22 23:01:50 -0700

    Remove breakage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :lockable, :timeoutable,
-         :password_expirable, :secure_validatable, :password_archivable,
-         :paranoid_verification
+         :password_expirable, :secure_validatable, :password_archivable
 end
