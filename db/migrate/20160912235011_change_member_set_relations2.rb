1403f36c205c66298d9112e4961e9811ebe7475e
Dan Brewster <dan@dbrews.com>
2016-10-05 00:19:56 -0700

    try try again migration

--- db/migrate/20160912235011_change_member_set_relations2.rb
+++ db/migrate/20160912235011_change_member_set_relations2.rb
@@ -1,6 +1,6 @@
 class ChangeMemberSetRelations2 < ActiveRecord::Migration[5.0]
   def change
-    add_foreign_key :set_member_instruments, :set_members
+    add_foreign_key :set_member_instruments, :set_member
     remove_column :set_member_instruments, :member_instrument_id
     remove_column :set_member_instruments, :set_status
     remove_column :set_member_instruments, :rotating
