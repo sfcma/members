f3db8e01d51935c84c338b435984ddc55bddc489
Dan Brewster <dan@dbrews.com>
2016-10-05 00:30:37 -0700

    Removing unused column removal

--- db/migrate/20160912235011_change_member_set_relations2.rb
+++ db/migrate/20160912235011_change_member_set_relations2.rb
@@ -3,6 +3,5 @@ class ChangeMemberSetRelations2 < ActiveRecord::Migration[5.0]
     remove_column :set_member_instruments, :member_instrument_id
     remove_column :set_member_instruments, :set_status
     remove_column :set_member_instruments, :rotating
-    remove_column :set_member_instruments, :set_id
   end
 end
