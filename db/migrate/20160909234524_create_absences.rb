ffdd856090b2ea015d568318bdc110140f35c90b
Dan Brewster <dan@dbrews.com>
2016-10-05 00:09:30 -0700

    Apparently I'm really bad at getting migrations right

--- db/migrate/20160909234524_create_absences.rb
+++ db/migrate/20160909234524_create_absences.rb
@@ -2,7 +2,7 @@ class CreateAbsences < ActiveRecord::Migration[5.0]
   def change
     create_table :absences do |t|
       t.references :member, foreign_key: true
-      t.references :performanceset, foreign_key: true
+      t.references :performance_set, foreign_key: true
       t.date :date
       t.boolean :planned
       t.boolean :sub_found
