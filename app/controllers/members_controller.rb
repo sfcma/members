a42a2284b23c57535c3da2bb08cf71c2224e4b78
Dan Brewster <dan@dbrews.com>
2016-10-05 01:04:55 -0700

    add date validation and impressionist gem

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,6 +2,7 @@ class MembersController < ApplicationController
   before_action :set_member, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   before_action :load_sets
+  impressionist
 
   # GET /members
   # GET /members.json
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,7 @@
 class Member < ApplicationRecord
   audited
   acts_as_paranoid
+  is_impressionable
   has_many :member_instruments
   has_many :member_sets
   has_many :member_notes
@@ -11,6 +12,8 @@ class Member < ApplicationRecord
   validates :first_name, presence: true
   validates_associated :member_sets, { message: -> (obj, data) { "Please include an instrument and set name on each set added." } }
   validates_associated :member_instruments, { message: -> (obj, data) { "uh oh" } }
+  validates_date :initial_date, allow_blank: true
+  validates_date :waiver_signed, allow_blank: true
 
   enum statuses: [:untriaged, :placed_in_group, :waitlist, :sub_only, :inactive]
 end
