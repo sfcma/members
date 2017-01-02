9ace20d10bc61c02354d6b651336d6f30e216966
Dan Brewster <dan@dbrews.com>
2017-01-02 11:13:39 -0800

    Fix mailer from address

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -3,7 +3,7 @@ class MemberMailer < ApplicationMailer
   def standard_member_email(user, subject, body)
     @user = user
     @body = body
-    mail(to: @user.email_1, subject: subject, body: @body)
+    mail(to: @user.email_1, from: 'membership@sfcivicsymphony.org', subject: subject, body: @body)
   end
 
 end
