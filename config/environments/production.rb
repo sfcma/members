984386e3cfa3052959015caa76b401ebbb4ac764
Dan Brewster <dan@dbrews.com>
2017-01-02 12:49:00 -0800

    Update mail domains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'members.sfcivicsymphony.org',
+    :domain         => 'mail.sfcivicmusic.org',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
