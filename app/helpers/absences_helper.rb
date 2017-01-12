d9c5f9046b6859fc560417d244b792649bfad69a
Dan Brewster <dan@dbrews.com>
2017-01-11 23:11:21 -0800

    Update absences table data

--- app/helpers/absences_helper.rb
+++ app/helpers/absences_helper.rb
@@ -3,12 +3,16 @@ module AbsencesHelper
   def absence_text(absence)
     text = ""
     if absence.planned
-      text = link_to "Excused", absence_url(absence)
+      text = link_to "Reported", absence_url(absence)
     else
-      text = link_to "Unexcused", absence_url(absence)
+      text = link_to "Unreported", absence_url(absence)
     end
 
-    if absence.sub_found
+    if absence.planned
+      text << "<br>Reported: #{absence.created_at.strftime('%b %e %H:%M')}"
+    end
+
+    if absence.sub_found.present?
       text << "<br><i>SUB: #{absence.sub_found}<i>".html_safe
     end
     text
