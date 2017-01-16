965b998f3ddc482993340824b3ce103360329391
Dan Brewster <dan@dbrews.com>
2017-01-16 00:08:48 -0800

    Better time display

--- app/helpers/absences_helper.rb
+++ app/helpers/absences_helper.rb
@@ -9,7 +9,7 @@ module AbsencesHelper
     end
 
     if absence.planned
-      text << "<br><i style='color: gray; font-size: 0.9em;'>Submitted #{absence.created_at.strftime('%b %e %H:%M')}</i>".html_safe
+      text << "<br><i style='color: gray; font-size: 0.9em;'>Submitted #{absence.created_at.strftime('%b %e %l:%M%P')}</i>".html_safe
     end
 
     if absence.sub_found.present?
