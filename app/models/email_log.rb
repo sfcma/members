237f9f4674d08d848bd601639f9675d9f32fd6b6
Dan Brewster <dan@dbrews.com>
2017-02-26 17:14:49 -0800

    Fix

--- app/models/email_log.rb
+++ app/models/email_log.rb
@@ -1,7 +1,8 @@
 class EmailLog < ApplicationRecord
   audited associated_with: :member
   acts_as_paranoid
-  has_one :member, :email
+  has_one :member
+  has_one :email
 
   validates :member, present: true
   validates :email, present: true
