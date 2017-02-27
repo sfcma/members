684a9a25a8e7da56249ac8b00c42d612563cdd3d
Dan Brewster <dan@dbrews.com>
2017-02-26 19:49:37 -0800

    Trying something

--- app/models/email_log.rb
+++ app/models/email_log.rb
@@ -2,7 +2,7 @@ class EmailLog < ApplicationRecord
   audited associated_with: :member
   acts_as_paranoid
   has_one :member
-  has_one :email
+  #has_one :email
 
   validates :member, present: true
   validates :email, present: true
