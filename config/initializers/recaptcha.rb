1f69eca52cf1bf72d976e8ecdcc57a2af13536e2
Dan Brewster <dan@dbrews.com>
2017-05-05 01:49:55 -0700

    Add missing files :/

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.site_key  = '6LffGCAUAAAAAIYMb1fKVLsU1oNJzhmssGyev-w9'
+  config.secret_key = '6LffGCAUAAAAAIBPluSV3u8fk4DYvb3VrNVBfeXy'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
