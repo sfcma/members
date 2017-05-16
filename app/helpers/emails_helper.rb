de9fb4374fd47a235666efecda939969d349b2ad
Dan Brewster <dan@dbrews.com>
2017-05-16 07:41:12 -0700

    Fix selection of ensembles to email

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -3,7 +3,7 @@ module EmailsHelper
     if current_user.global_admin?
       return PerformanceSet.all.emailable
     else
-      return PerformanceSet.where(ensemble_id: UserEnsemble.where(user_id: current_user.id)).emailable
+      return PerformanceSet.where(ensemble_id: UserEnsemble.where(user_id: current_user.id).map(&:ensemble_id)).emailable
     end
   end
 end
