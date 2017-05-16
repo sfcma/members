b32d2ae2af311552bd18aa319c7283fb7cd0c176
Dan Brewster <dan@dbrews.com>
2017-05-15 23:10:42 -0700

    Add misplaced helper

---
+++ app/helpers/emails_helper.rb
@@ -0,0 +1,9 @@
+module EmailsHelper
+  def emailable_performance_sets
+    if current_user.global_admin?
+      return PerformanceSet.all.emailable
+    else
+      return PerformanceSet.where(ensemble_id: UserEnsemble.where(user_id: current_user.id)).emailable
+    end
+  end
+end
