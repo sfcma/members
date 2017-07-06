e4fc7353d36e504aa268a13346165f1077d293f8
Dan Brewster <dan@dbrews.com>
2017-07-06 00:28:56 -0700

    Add forgotten lib file

---
+++ lib/instruments.rb
@@ -0,0 +1,33 @@
+module Instruments
+
+  # also in JavaScript, though capitalized and without un-suffixed 'Violin'
+  def self.instruments
+    [ 'piccolo',
+      'flute',
+      'oboe',
+      'b flat clarinet',
+      'e flat clarinet',
+      'bass clarinet',
+      'english horn',
+      'bassoon',
+      'contrabassoon',
+      'alto saxophone',
+      'tenor saxophone',
+      'trumpet',
+      'french horn',
+      'trombone',
+      'bass trombone',
+      'tuba',
+      'timpani',
+      'percussion',
+      'piano',
+      'harp',
+      'violin',
+      'violin 1',
+      'violin 2',
+      'viola',
+      'cello',
+      'string bass'
+    ]
+  end
+end
