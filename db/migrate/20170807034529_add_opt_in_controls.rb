3833eb864b340b3fb20c17b5cc3f3483cd82daab
Dan Brewster <dan@dbrews.com>
2017-08-12 02:22:45 -0700

    different envs is just bad

--- db/migrate/20170807034529_add_opt_in_controls.rb
+++ db/migrate/20170807034529_add_opt_in_controls.rb
@@ -2,6 +2,6 @@ class AddOptInControls < ActiveRecord::Migration[5.0]
   def change
     add_column :performance_set_instruments, :available_to_opt_in, :boolean
     add_column :performance_set_instruments, :opt_in_message_type, :string
-    add_column :performance_set_instruments, :opt_in_message_id, :number
+    add_column :performance_set_instruments, :opt_in_message_id, :integer
   end
 end
