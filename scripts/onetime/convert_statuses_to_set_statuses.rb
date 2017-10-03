a4fec2b975480ecd68289ce1378c0e11bd7b3950
Dan Brewster <dan@dbrews.com>
2017-10-03 01:22:32 -0700

    Fix script

--- scripts/onetime/convert_statuses_to_set_statuses.rb
+++ scripts/onetime/convert_statuses_to_set_statuses.rb
@@ -2,19 +2,19 @@ def convert_statuses
   MemberSet.all.each do |ms|
     case ms.set_status
     when 'Interested in playing this set, unconfirmed'
-      ms.update(set_status_id: :interested)
+      ms.update(set_status: :interested)
     when 'playing'
-      ms.update(set_status_id: :performing)
+      ms.update(set_status: :performing)
     when 'Stopped playing, due to member\'s own decision'
-      ms.update(set_status_id: :stopped_by_self)
+      ms.update(set_status: :stopped_by_self)
     when 'Unable to play, not due to Member\'s decision'
-      ms.update(set_status_id: :stopped_by_us)
+      ms.update(set_status: :stopped_by_us)
     when 'subbing'
-      ms.update(set_status_id: :substituting)
+      ms.update(set_status: :substituting)
     when 'Not Interested in this set'
-      ms.update(set_status_id: :uninterested)
+      ms.update(set_status: :uninterested)
     when 'Opted in for this set'
-      ms.update(set_status_id: :interested)
+      ms.update(set_status: :interested)
     end
   end
 end
