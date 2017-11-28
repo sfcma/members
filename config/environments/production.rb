4d660dec62346bd4bf46134496581bdd03e5587f
Dan Brewster <dan@dbrews.com>
2017-11-27 20:45:30 -0800

    update email address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'mail.sfcivicmusic.org',
+    :domain         => 'membermail.sfcivicmusic.org',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
